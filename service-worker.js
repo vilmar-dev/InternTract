a3e9ae46982d6706206ca83c312b306fdfbf24d3
Vilmar Jr Ammasi <ammasivilmar2@gmail.com>
2026-08-17 00:09:33 +0800

    Update service worker to version 3 and optimize caching

--- service-worker.js
+++ service-worker.js
@@ -1,71 +1,48 @@
-const CACHE_NAME = "interntrack-v1";
+const CACHE_NAME = "interntrack-v3";
 
-const FILES_TO_CACHE = [
+const APP_FILES = [
   "./",
   "./index.html",
   "./style.css",
-
-  "./login.html",
-  "./login.css",
-  "./login.js",
-
-  "./intern.html",
-  "./intern.css",
-  "./intern.js",
-
-  "./admin.html",
-  "./admin.css",
-  "./admin.js",
-
-  "./register.html",
-  "./register.js",
-
-  "./pending.html",
-
-  "./auth.js",
-  "./firebase.js",
-  "./utils.js",
-
   "./manifest.json",
-  "./macro.png",
-
   "./interntrack-icon-192x192.png",
   "./interntrack-icon-512x512.png"
 ];
 
-// Install
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+      return cache.addAll(APP_FILES);
     })
   );
 
   self.skipWaiting();
 });
 
-// Activate
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
+    caches.keys().then((names) =>
+      Promise.all(
+        names
           .filter((name) => name !== CACHE_NAME)
           .map((name) => caches.delete(name))
-      );
-    })
+      )
+    )
   );
 
   self.clients.claim();
 });
 
-// Fetch
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
